cdfcfb1e4b17973f71016c6ab38ea6ad34157da7
Steve Nolen <technolengy@gmail.com>
2026-03-18 16:01:43 -0400

    feat: pre-patch Installation CRD to allow Nftables dataplane on upgrade
    The Nftables linuxDataplane value was added in Calico 3.31, but older
    CRD schemas only allow Iptables/BPF/VPP. Patch the CRD enum before
    the Helm release so the API server accepts the new value, enabling
    single-step upgrades from older versions.

--- python-pulumi/src/ptd/pulumi_resources/tigera_operator.py
+++ python-pulumi/src/ptd/pulumi_resources/tigera_operator.py
@@ -29,6 +29,7 @@ class TigeraOperator(pulumi.ComponentResource):
 
         self._define_namespace()
 
+        self._patch_installation_crd()
         self._adopt_felix_configuration()
 
         self._define_helm_release()
@@ -47,6 +48,62 @@ class TigeraOperator(pulumi.ComponentResource):
                 name="tigera-operator",
             ),
             opts=pulumi.ResourceOptions(parent=self),
+        )
+
+    def _patch_installation_crd(self):
+        """Patch the Installation CRD to add Nftables to the linuxDataplane enum.
+
+        The Nftables dataplane was added in Calico 3.31, but the old CRD schema
+        only allows Iptables/BPF/VPP. This patch must run before the Helm release
+        so the API server accepts the new value during upgrade.
+        """
+        self._installation_crd_patch = k8s.apiextensions.v1.CustomResourceDefinitionPatch(
+            f"{self.name}-{self.release}-installation-crd-patch",
+            metadata=k8s.meta.v1.ObjectMetaPatchArgs(
+                name="installations.operator.tigera.io",
+            ),
+            spec=k8s.apiextensions.v1.CustomResourceDefinitionSpecPatchArgs(
+                group="operator.tigera.io",
+                names=k8s.apiextensions.v1.CustomResourceDefinitionNamesPatchArgs(
+                    plural="installations",
+                    singular="installation",
+                    kind="Installation",
+                ),
+                versions=[
+                    k8s.apiextensions.v1.CustomResourceDefinitionVersionPatchArgs(
+                        name="v1",
+                        served=True,
+                        storage=True,
+                        schema=k8s.apiextensions.v1.CustomResourceValidationPatchArgs(
+                            open_apiv3_schema=k8s.apiextensions.v1.JSONSchemaPropsPatchArgs(
+                                type="object",
+                                properties={
+                                    "spec": k8s.apiextensions.v1.JSONSchemaPropsPatchArgs(
+                                        type="object",
+                                        properties={
+                                            "calicoNetwork": k8s.apiextensions.v1.JSONSchemaPropsPatchArgs(
+                                                type="object",
+                                                properties={
+                                                    "linuxDataplane": k8s.apiextensions.v1.JSONSchemaPropsPatchArgs(
+                                                        type="string",
+                                                        enum=[
+                                                            "Iptables",
+                                                            "BPF",
+                                                            "VPP",
+                                                            "Nftables",
+                                                        ],
+                                                    ),
+                                                },
+                                            ),
+                                        },
+                                    ),
+                                },
+                            ),
+                        ),
+                    ),
+                ],
+            ),
+            opts=pulumi.ResourceOptions(parent=self, depends_on=self.namespace),
         )
 
     def _adopt_felix_configuration(self):
@@ -72,7 +129,7 @@ class TigeraOperator(pulumi.ComponentResource):
         )
 
     def _define_helm_release(self):
-        helm_depends = [self.namespace, self._felix_patch]
+        helm_depends = [self.namespace, self._installation_crd_patch, self._felix_patch]
 
         self.helm_release = k8s.helm.v3.Release(
             f"{self.name}-{self.release}-tigera-operator",
