c1db0c50c4a4c18aca29a386819ccbd79a4a22cb
Steve Nolen <technolengy@gmail.com>
2026-03-18 08:08:23 -0400

    fix: adopt existing FelixConfiguration before Helm manages it
    When disabling third-party telemetry, the Tigera Helm chart needs to
    manage the existing default FelixConfiguration. Add a CustomResourcePatch
    to set Helm ownership labels/annotations before the Helm release runs,
    allowing Helm to adopt the pre-existing resource.

--- python-pulumi/src/ptd/pulumi_resources/tigera_operator.py
+++ python-pulumi/src/ptd/pulumi_resources/tigera_operator.py
@@ -28,6 +28,10 @@ class TigeraOperator(pulumi.ComponentResource):
         self.third_party_telemetry_enabled = third_party_telemetry_enabled
 
         self._define_namespace()
+
+        if not self.third_party_telemetry_enabled:
+            self._adopt_felix_configuration()
+
         self._define_helm_release()
 
         self.register_outputs(
@@ -46,7 +50,33 @@ class TigeraOperator(pulumi.ComponentResource):
             opts=pulumi.ResourceOptions(parent=self),
         )
 
+    def _adopt_felix_configuration(self):
+        """Add Helm ownership labels/annotations to the existing default FelixConfiguration.
+
+        The Tigera Operator creates a default FelixConfiguration on install. When we enable
+        defaultFelixConfiguration in the Helm values, Helm needs ownership metadata on the
+        existing resource to adopt it.
+        """
+        self._felix_patch = k8s.apiextensions.CustomResourcePatch(
+            f"{self.name}-{self.release}-felix-helm-adopt",
+            api_version="crd.projectcalico.org/v1",
+            kind="FelixConfiguration",
+            metadata=k8s.meta.v1.ObjectMetaPatchArgs(
+                name="default",
+                labels={"app.kubernetes.io/managed-by": "Helm"},
+                annotations={
+                    "meta.helm.sh/release-name": "tigera-operator",
+                    "meta.helm.sh/release-namespace": "tigera-operator",
+                },
+            ),
+            opts=pulumi.ResourceOptions(parent=self, depends_on=self.namespace),
+        )
+
     def _define_helm_release(self):
+        helm_depends = [self.namespace]
+        if hasattr(self, "_felix_patch"):
+            helm_depends.append(self._felix_patch)
+
         self.helm_release = k8s.helm.v3.Release(
             f"{self.name}-{self.release}-tigera-operator",
             chart="tigera-operator",
@@ -94,5 +124,5 @@ class TigeraOperator(pulumi.ComponentResource):
                     else {}
                 ),
             },
-            opts=pulumi.ResourceOptions(parent=self, depends_on=self.namespace),
+            opts=pulumi.ResourceOptions(parent=self, depends_on=helm_depends),
         )
